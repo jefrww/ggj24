d3af566f24bc6d59df631fcf0c05b01517f24582
Jan-Erik Wieczorek <jan-erik.wieczorek@freiheit.com>
2024-01-27 15:21:22 +0100

    Add basic movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,15 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+enum Movement 
+{
+    Horizontal,
+    Vertical,
+    None
+}
+
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed;
+    public float horizontalSpeed, verticalSpeed;
     public float jumpSpeed;
     public float groundDistance, wallDistance;
+    public float maxVelocity;
+    public float aircontrol;
     
     private Rigidbody2D rb;
-    private bool hasTorso= false,hasLegs= false,hasArms= false,hasHead= false, hasJaw= false;
+    private bool hasTorso= true,hasLegs= true,hasArms= true,hasHead= true, hasJaw= true;
     private bool _onGround= false,_onLeftWall= false, _onRightWall = false;
 
 
@@ -23,26 +33,57 @@ public class PlayerMovement : MonoBehaviour
         // get current state of the player first
         _onGround = IsTouchingGround();
         (_onLeftWall, _onRightWall) = IsTouchingWalls();
+        Debug.Log("Ground: " +_onGround);
+        Debug.Log("Left Wall: " +_onLeftWall);
+        Debug.Log("Right Wall: "+_onRightWall);
+
         Vector3 velocity = Vector3.zero;
+        Movement movement = Movement.None;
         // get input
         if (Input.GetKey(KeyCode.A) && canGoLeft())
         {
+            movement = Movement.Horizontal;
             velocity += Vector3.left;
         }
         if (Input.GetKey(KeyCode.D) && canGoRight())
         {
+            movement = Movement.Horizontal;
             velocity += Vector3.right;
         }
         if (Input.GetKey(KeyCode.W) && canWallClimb())
         {
+            movement = Movement.Vertical;
             velocity += Vector3.up;
         }
         if (Input.GetKey(KeyCode.S) && canWallClimb())
         {
+            movement = Movement.Vertical;
             velocity += Vector3.down;
         }
+        // normalize change in speed to ensure consistency
+        velocity = velocity.normalized;
+        
+        
+        
+        // apply movement
+        switch (movement)
+        {
+            case Movement.Vertical:
+                velocity *= verticalSpeed;
+                break;
+            case Movement.Horizontal:
+                velocity *= horizontalSpeed;
+                break;
+            case Movement.None:
+                break;
+        }
+        
+        // reduce air control
+        if (isInAir())
+        {
+            velocity *= aircontrol;
+        }
 
-        velocity = velocity.normalized * moveSpeed;
         rb.AddForce(velocity);
         
         if (Input.GetKeyDown(KeyCode.Space) && canJump())
@@ -50,7 +91,16 @@ public class PlayerMovement : MonoBehaviour
             rb.AddForce(Vector2.up * jumpSpeed);
         }
     }
-    
+
+    private void FixedUpdate()
+    {
+        Debug.Log(rb.velocity.sqrMagnitude);
+        if( rb.velocity.sqrMagnitude > maxVelocity )
+        {
+            rb.velocity *= 0.75f;
+        }
+    }
+
     bool IsTouchingGround()
     {
         RaycastHit2D hit = Physics2D.Raycast(rb.transform.position, Vector3.down, groundDistance, LayerMask.GetMask("Ground"));
@@ -83,27 +133,31 @@ public class PlayerMovement : MonoBehaviour
 
     bool canGoLeft()
     {
-        return hasTorso && hasArms && hasHead && hasJaw;
+        return hasTorso || hasArms || hasHead || hasJaw;
     }
 
     bool canGoRight()
     {
-        return hasTorso && hasArms && hasHead && hasJaw;
+        return hasTorso || hasArms || hasHead || hasJaw;
     }
 
     bool canJump()
     {
-        return hasLegs;
+        return hasLegs && (_onGround);
     }
 
     bool canWallClimb()
     {
-        return hasArms;
+        return hasArms && (_onRightWall || _onLeftWall);
     }
 
     bool canLaser()
     {
         return  hasHead ;
+    }
+    bool isInAir()
+    {
+        return !(_onRightWall || _onLeftWall || _onGround) ;
     }
     
 }
