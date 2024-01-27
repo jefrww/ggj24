a26f49ce304a7c979e04fe46200abebc63519513
Andy Michael <andy.michael@revolution.dev>
2024-01-27 16:28:26 +0100

    add tile map

---
+++ Assets/TileSource/CityTiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="CiryTiles" tilewidth="32" tileheight="32" tilecount="81" columns="9">
+ <image source="Art/1 Tiles/Tileset.png" width="288" height="288"/>
+</tileset>
