7b78a4bf9175add5935b9f69b09492734bc2f9df
Jan-Erik Wieczorek <jan-erik.wieczorek@freiheit.com>
2024-01-27 13:26:31 +0100

    Define movement options

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,49 +4,106 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private Rigidbody2D rb;
-
     public float moveSpeed;
     public float jumpSpeed;
-    public float groundDistance;
+    public float groundDistance, wallDistance;
+    
+    private Rigidbody2D rb;
+    private bool hasTorso= false,hasLegs= false,hasArms= false,hasHead= false, hasJaw= false;
+    private bool _onGround= false,_onLeftWall= false, _onRightWall = false;
+
+
     void Awake()
     {
         rb = this.GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
-    {
+    {   
+        // get current state of the player first
+        _onGround = IsTouchingGround();
+        (_onLeftWall, _onRightWall) = IsTouchingWalls();
         Vector3 velocity = Vector3.zero;
-        if (Input.GetKey(KeyCode.A))
+        // get input
+        if (Input.GetKey(KeyCode.A) && canGoLeft())
         {
             velocity += Vector3.left;
         }
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.D) && canGoRight())
         {
             velocity += Vector3.right;
+        }
+        if (Input.GetKey(KeyCode.W) && canWallClimb())
+        {
+            velocity += Vector3.up;
+        }
+        if (Input.GetKey(KeyCode.S) && canWallClimb())
+        {
+            velocity += Vector3.down;
         }
 
         velocity = velocity.normalized * moveSpeed;
         rb.AddForce(velocity);
         
-        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
+        if (Input.GetKeyDown(KeyCode.Space) && canJump())
         {
-            Debug.Log(IsGrounded());
             rb.AddForce(Vector2.up * jumpSpeed);
         }
     }
     
-    bool IsGrounded()
+    bool IsTouchingGround()
     {
-        Debug.Log("RB: " + rb.transform.position);
-        RaycastHit2D hit = Physics2D.Raycast(rb.transform.position + Vector3.up, Vector3.down, groundDistance, LayerMask.GetMask("Ground"));
-        Debug.Log("Hit: " +hit.point);
+        RaycastHit2D hit = Physics2D.Raycast(rb.transform.position, Vector3.down, groundDistance, LayerMask.GetMask("Ground"));
         if (hit.collider == null)
         {
-            Debug.Log("NO HIT");
             return false;
         }
         return hit.collider.CompareTag("Ground");
     }
+    
+    (bool left, bool right) IsTouchingWalls()
+    {
+        Vector3 position = rb.transform.position;
+        bool touchingLeft = false, touchingRight = false;
+        RaycastHit2D leftHit = Physics2D.Raycast(position, Vector3.left, wallDistance, LayerMask.GetMask("Wall"));
+        RaycastHit2D rightHit = Physics2D.Raycast(position, Vector3.right, wallDistance, LayerMask.GetMask("Wall"));
+
+        if (leftHit.collider != null && leftHit.collider.CompareTag("Wall"))
+        {
+            touchingLeft = true;
+        }
+        
+        if (rightHit.collider != null && rightHit.collider.CompareTag("Wall"))
+        {
+            touchingRight = true;
+        }
+
+        return (touchingLeft, touchingRight);
+    }
+
+    bool canGoLeft()
+    {
+        return hasTorso && hasArms && hasHead && hasJaw;
+    }
+
+    bool canGoRight()
+    {
+        return hasTorso && hasArms && hasHead && hasJaw;
+    }
+
+    bool canJump()
+    {
+        return hasLegs;
+    }
+
+    bool canWallClimb()
+    {
+        return hasArms;
+    }
+
+    bool canLaser()
+    {
+        return  hasHead ;
+    }
+    
 }
