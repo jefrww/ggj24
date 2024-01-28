68c640c0bc0ba04fb69766a4231496c1c4be9b0f
Andy Michael <andy.michael@revolution.dev>
2024-01-28 16:36:27 +0100

    Melt

--- Assets/TileSource/Robotset.tsx
+++ Assets/TileSource/Robotset.tsx
@@ -148,11 +148,6 @@
    <object id="1" x="0" y="0" width="32" height="32"/>
   </objectgroup>
  </tile>
- <tile id="31">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="0" width="32" height="32"/>
-  </objectgroup>
- </tile>
  <tile id="36">
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="0" width="32" height="32"/>
